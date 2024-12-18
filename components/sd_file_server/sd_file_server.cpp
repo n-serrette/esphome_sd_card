cb93c4cc2814159b73880bdb47bad2af4156f252
n-serrette <serrette.nicola@gmail.com>
2024-12-18 18:37:31 +0100

    webserver show root files

--- components/sd_file_server/sd_file_server.cpp
+++ components/sd_file_server/sd_file_server.cpp
@@ -15,23 +15,46 @@ void SDFileServer::setup() { this->base_->add_handler(this); }
 void SDFileServer::dump_config() {
   ESP_LOGCONFIG(TAG, "SD File Server:");
   ESP_LOGCONFIG(TAG, "  Address: %s:%u", network::get_use_address().c_str(), this->base_->get_port());
-  ESP_LOGCONFIG(TAG, "  Url Prefix: %s", this->url_prefix_);
+  ESP_LOGCONFIG(TAG, "  Url Prefix: %s", this->url_prefix_.c_str());
 }
 
 bool SDFileServer::canHandle(AsyncWebServerRequest *request) {
-  return str_startswith(std::string(request->url().c_str()), this->url_prefix_);
+  ESP_LOGD(TAG, "can handle %s %u", request->url().c_str(),
+           str_startswith(std::string(request->url().c_str()), this->buildPrefix()));
+  return str_startswith(std::string(request->url().c_str()), this->buildPrefix());
 }
 
 void SDFileServer::handleRequest(AsyncWebServerRequest *request) {
   ESP_LOGD(TAG, request->url().c_str());
-  if (str_startswith(std::string(request->url().c_str()), this->url_prefix_)) {
-    auto *response = request->beginResponse(200, "text/plain", "Hello World!");
-    request->send(response);
+  if (str_startswith(std::string(request->url().c_str()), this->buildPrefix())) {
+    this->handleIndex(request);
     return;
   }
 }
 
 void SDFileServer::set_url_prefix(std::string const &prefix) { this->url_prefix_ = prefix; }
+
+void SDFileServer::set_root_path(std::string const &path) { this->root_path_ = path; }
+
+void SDFileServer::set_sd_mmc_card(sd_mmc_card::SdMmc *card) { this->sd_mmc_card_ = card; }
+
+void SDFileServer::handleIndex(AsyncWebServerRequest *request) {
+  auto *response = request->beginResponseStream("text/html");
+  response->print("<h1>SD Card Content</h1>");
+  auto entries = this->sd_mmc_card_->list_directory(this->root_path_.c_str(), 0);
+  for (auto const &entry : entries) {
+    response->print("<span>");
+    response->print(entry.c_str());
+    response->print("</span></br>");
+  }
+  request->send(response);
+}
+
+std::string SDFileServer::buildPrefix() const {
+  if (this->url_prefix_.length() == 0 || this->url_prefix_.at(0) != '/')
+    return "/" + this->url_prefix_;
+  return this->url_prefix_;
+}
 
 }  // namespace sd_file_server
 }  // namespace esphome
